9821a92a59695e4598278540ed0837d1ab60d718
Manish Sonawane <manishsonawane3010@gmail.com>
2025-12-04 17:46:06 +0000

    Module not found: Can't resolve 'request'
    ./node_modules/useragent/lib/

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,5 @@
 // Please add your MongoDB connection string here
 export const MONGODB_URI = process.env.MONGODB_URI || "";
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET || "";
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY || "";
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME || "";
